8eb806221458e21bbbc3833ab0c802f8c48f83e9
bibvingenieria <bibv.ingenieria@urp.edu.pe>
2026-06-02 19:08:14 +0000

    Avance Respectivo

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://decigqwopvoonmbhvmcn.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRlY2lncXdvcHZvb25tYmh2bWNuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgxMjA4MjYsImV4cCI6MjA3MzY5NjgyNn0.ykzy6selTiR5cZe1pvNoniPRBegr5pxyOp8L0n3uyI8'
+const supabaseUrl = 'https://mltcrzpphrlczkoyxgcc.supabase.co'
+const supabaseKey = 'sb_publishable_QTFTM79hIjwFSk6LLAoL1g_DiRWNACv'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
